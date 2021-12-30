21981fd17606f8aecfaa9ef464409422c23407a5
Kirill <foxxx2game@gmail.com>
2021-12-30 12:29:01 +0300

    rebase auth to another project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import AuthWrapper from "./hoc/AuthWrapper/AuthWrapper";
 import BarsWrapper from "./hoc/BarsWrapper/BarsWrapper";
 import Dashboard from "./pages/Dashboard";
-import WelcomeForm from "./components/Auth/WelcomeForm/WelcomeForm";
-import LoginForm from "./components/Auth/LoginForm/LoginForm";
-import RegisterForm from "./components/Auth/RegisterForm/RegisterForm";
 
 function App() {
 	return (
 		<Routes>
 			<Route
 				path="/"
-				element={<AuthWrapper wrappedComponent={<WelcomeForm />} />}
-			/>
-			<Route
-				path="/auth/login"
-				element={<AuthWrapper wrappedComponent={<LoginForm />} />}
-			/>
-			<Route
-				path="/auth/register"
-				element={<AuthWrapper wrappedComponent={<RegisterForm />} />}
-			/>
-			<Route
-				path="/dash"
 				element={<BarsWrapper wrappedComponent={<Dashboard />} />}
 			/>
 		</Routes>
